ecd5c893a7f07f8bd91ebafc3b68895138fd000d
Shithead Dev <shddev@novactorio.network>
2026-05-21 15:37:57 +0200

    chore: add build info marker for Cloudflare debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { AuthGuard } from './services/auth/AuthGuard';
-import { AuthService } from './services/auth/AuthService';
 import './index.css';
+import './debug-build';
 
 AuthService.init();
 
@@ -14,3 +13,4 @@ createRoot(document.getElementById('root')!).render(
     </AuthGuard>
   </StrictMode>
 );
+import './debug-build';
